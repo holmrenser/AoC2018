4545984556ab5faa64700b797ca3cf034f48fec8
Rens Holmer <rens.holmer@wur.nl>
2018-12-03 15:03:17 +0100

    2

---
+++ 2.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+fs.readFile('./2.input', (err, data) => {
+  let foundDuplicate = false;
+  let frequency = 0;
+  const seenFrequencies = new Set();
+  const changes = data.toString('utf8').split('\n').map(d => {
+    const sign = d[0];
+    const magnitude = parseInt(d.slice(1));
+    return [sign, magnitude];
+  });
+  while (!foundDuplicate) {
+    frequency = changes.reduce((acc, [sign, magnitude]) => {
+      if (sign === '+'){
+        acc += magnitude
+      } else {
+        acc -= magnitude
+      }
+
+      if (seenFrequencies.has(acc)) {
+        if (!foundDuplicate) {
+          console.log(acc)
+        }
+        foundDuplicate = true;
+      }
+      seenFrequencies.add(acc)
+
+      return acc
+    }, frequency)
+    seenFrequencies.add(frequency)
+    // frequency = newFrequency
+  }
+})
